f7a817ce9ed4ef1cf08041c6fa7fcda69d8bc040
muhammad-magdi <muhammad.magdi1024@gmail.com>
2021-07-21 22:16:37 +0200

    re-format using clang formatter

--- headers/ImageProcessor.hpp
+++ headers/ImageProcessor.hpp
@@ -1,10 +1,10 @@
 #ifndef IMAGE_PROCESSOR_H
 #define IMAGE_PROCESSOR_H
 
+#include "bitmap_image.hpp"
 #include <algorithm>
 #include <iostream>
 #include <vector>
-#include "bitmap_image.hpp"
 
 using namespace std;
 
@@ -16,30 +16,31 @@ using namespace std;
 
 typedef unsigned int u_int32;
 
-class ImageProcessor{
+class ImageProcessor {
 public:
-  struct Pair{
-    Pair(){}
-    Pair(int _first, int _second)
-    : first(_first),
-    second(_second)
-    {}
-      int first, second;
-  };
+    struct Pair {
+        Pair() { }
+        Pair(int _first, int _second)
+            : first(_first)
+            , second(_second)
+        {
+        }
+        int first, second;
+    };
 
-  Image applyThreshold(Image image, const int& th);
-  Image applyMedianFilter(Image image);
-  Image applyErosion(Image image);
-  Image applyDilation(Image image);
-  Pair hitter(const Image& image, u_int32 x, u_int32 y);
-  Pair structuringElement3(const Image& image, u_int32 x, u_int32 y);
+    Image applyThreshold(Image image, const int& th);
+    Image applyMedianFilter(Image image);
+    Image applyErosion(Image image);
+    Image applyDilation(Image image);
+    Pair hitter(const Image& image, u_int32 x, u_int32 y);
+    Pair structuringElement3(const Image& image, u_int32 x, u_int32 y);
 
+    inline static bool validPoint(const Image& image, const u_int32& x, const u_int32& y)
+    {
+        return x >= 0 && x < image.width() && y >= 0 && y < image.height();
+    }
 
-  inline static bool validPoint(const Image& image, const u_int32& x, const u_int32& y){
-    return x >= 0 && x < image.width() && y >= 0 && y < image.height();
-  }
 private:
-
 };
 
 #endif
--- headers/Pattern.hpp
+++ headers/Pattern.hpp
@@ -9,34 +9,36 @@
 
  default all zeros pattern
 */
-class Pattern{
+class Pattern {
 public:
-  Pattern(int _size, const int**& _pattern){
-    size = _size;
-    pattern = new int*[size];
-    for(int r = 0 ; r < size ; ++r){
-      pattern[r] = new pattern[_size];
-      for(int c = 0 ; c < size ; ++c)
-        pattern[r][c] = _pattern[r][c];
+    Pattern(int _size, const int**& _pattern)
+    {
+        size = _size;
+        pattern = new int*[size];
+        for (int r = 0; r < size; ++r) {
+            pattern[r] = new pattern[_size];
+            for (int c = 0; c < size; ++c)
+                pattern[r][c] = _pattern[r][c];
+        }
     }
-  }
 
-  Pattern(const int& _size,const unsigned char& pattern, const int& mainV = 1, const int& secV = 0){
-    size = _size;
-    pattern = new int*[size];
-    switch (/* expression */) {
-      case /* value */:
+    Pattern(const int& _size, const unsigned char& pattern, const int& mainV = 1, const int& secV = 0)
+    {
+        size = _size;
+        pattern = new int*[size];
+        switch (/* expression */) {
+        case /* value */:
+        }
     }
-  }
 
-  toPlusPattern(const int& mainV = 1, const int& secV = 0);
-  toMainDiagonalPattern(const int& mainV = 1, const int& secV = 0);
-  toSecDiagonalPattern(const int& mainV = 1, const int& secV = 0);
-  toOnesPattern();
-  
+    toPlusPattern(const int& mainV = 1, const int& secV = 0);
+    toMainDiagonalPattern(const int& mainV = 1, const int& secV = 0);
+    toSecDiagonalPattern(const int& mainV = 1, const int& secV = 0);
+    toOnesPattern();
+
 private:
-  int size;
-  int** pattern;
+    int size;
+    int** pattern;
 };
 
 #endif
